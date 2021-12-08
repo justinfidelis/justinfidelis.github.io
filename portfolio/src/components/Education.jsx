e129b7f08f0fad6f33c8c98bb8f925cc39847d0e
Justin Wong <justinfidelis@gmail.com>
2021-12-09 03:05:29 +0800

    Added skills and education info

--- portfolio/src/components/Education.jsx
+++ portfolio/src/components/Education.jsx
@@ -6,8 +6,27 @@ const Education = () => {
             <h2>Education</h2>
             <hr/>
             <div className="section-text">
-                <h3 style={{paddingBottom: "8px"}}>National University of Singapore</h3>
-                <small>August 2019 - Present</small>
+                <div className="education-container">
+                    <h3 style={{paddingBottom: "8px"}}>National University of Singapore</h3>
+                    <small style={{paddingBottom: "6px"}}>August 2020 - Present (Graduating in May 2024)</small>
+                    <div className="education-text">
+                        B.Eng. in Computer Engineering, Honours <br/>
+                        Specialisation in Internet of Things <br/>
+                        Awarded NUS Merit Scholarship <br/>
+                        Current CAP: /5.0
+                    </div>
+                </div>
+
+                <div className="education-container">
+                    <h3 style={{paddingBottom: "8px"}}>NUS High School of Math and Science</h3>
+                    <small style={{paddingBottom: "6px"}}>August 2012 - 2017</small>
+                    <div className="education-text">
+                    NUS High School Diploma (High Distinction) <br/> 
+                    Honours in Chemistry and Physics, and Major in Mathematics<br/>
+                    Graduation CAP: 4.6/5.0
+                    </div>
+
+                </div>
             </div>
 
 
--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -7,14 +7,33 @@ const Skills = () => {
             <hr/>
             <div className="skills-container">
                 <div className="skills-card">
-                    <h3 className="card-title">Languages</h3>
-                    
+                    <h3 className="card-title">Programming Languages</h3>
+                    <hr className="card-break" />
+                    <ul className="skills-list">
+                        <li className="skills-list-item">C</li>
+                        <li className="skills-list-item">C++</li>
+                        <li className="skills-list-item">Java</li>
+                        <li className="skills-list-item">Python</li>
+                        <li className="skills-list-item">HTML + CSS + JS</li>
+                    </ul>
                 </div>
                 <div className="skills-card">
                     <h3 className="card-title">Database Management</h3>
+                    <hr className="card-break" />
+                    <ul className="skills-list">
+                        <li className="skills-list-item">PostgreSQL</li>
+                        <li className="skills-list-item">MySQL</li>
+                    </ul>
                 </div>
                 <div className="skills-card">
-                    <h3 className="card-title">Hardware</h3>
+                    <h3 className="card-title">Hardware Technologies</h3>
+                    <hr className="card-break" />
+                    <ul className="skills-list">
+                        <li className="skills-list-item">Verilog</li>
+                        <li className="skills-list-item">Arduino</li>
+                        <li className="skills-list-item">ARM</li>
+                        <li className="skills-list-item">ROS</li>
+                    </ul>
                 </div>
             </div>
         </div>
