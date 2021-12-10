86e8d8dca166e08655934a7fe48039ca35d92261
Justin Wong <justinfidelis@gmail.com>
2021-12-10 16:49:02 +0800

    add reactive header

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import MainLogo from './MainLogo';
 
 const Header = () => {
+    const [prevScrollPos, setPrevScrollPos] = useState(0);
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        let handleScroll = () => {
+            const currentScrollPos = window.pageYOffset;
+
+            setVisible(currentScrollPos < prevScrollPos || currentScrollPos < 10);
+
+            setPrevScrollPos(currentScrollPos);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        console.log("useeffect triggered");
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [prevScrollPos]);
+
     return (
-    <div id="header">
+    <div id="header" style={{top: visible ? "0" : "-68px"}}>
         <div id="header-content">
             <MainLogo />
             <ul id="header-list">
