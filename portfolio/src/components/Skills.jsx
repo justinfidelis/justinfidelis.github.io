ca14e622528135fe2a029cc3f383fed82b48593f
Justin Wong <justinfidelis@gmail.com>
2023-07-14 16:24:24 +0800

    change skills to grid

--- portfolio/src/components/Skills.jsx
+++ portfolio/src/components/Skills.jsx
@@ -13,86 +13,55 @@ const Skills = () => {
         <Row className="justify-content-center">
           <hr className="subtitle-break" />
         </Row>
-        <Row>
-          <Col className="d-flex justify-content-center">
-            <Card className="skill-card">
-              <Card.Body className="pt-2 px-2">
-                <Card.Title>
-                  <h5 className="card-title">Programming Languages</h5>
-                </Card.Title>
-                <div className="d-flex justify-content-center">
-                  <hr className="card-break" />
-                </div>
-                <Card.Text>
-                  <ul className="skills-list">
-                    <li className="skills-li">C</li>
-                    <li className="skills-li">C++</li>
-                    <li className="skills-li">Java</li>
-                    <li className="skills-li">Python</li>
-                  </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="d-flex justify-content-center">
-            <Card className="skill-card">
-              <Card.Body className="pt-2 px-2">
-                <Card.Title>
-                  <h5 className="card-title">Hardware Technologies</h5>
-                </Card.Title>
-                <div className="d-flex justify-content-center">
-                  <hr className="card-break" />
-                </div>
-                <Card.Text>
-                  <ul className="skills-list">
-                    <li className="skills-li">Verilog</li>
-                    <li className="skills-li">Arduino</li>
-                    <li className="skills-li">Raspberry Pi</li>
-                    <li className="skills-li">ARM</li>
-                    <li className="skills-li">ROS</li>
-                  </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="d-flex justify-content-center">
-            <Card className="skill-card">
-              <Card.Body className="pt-2 px-2">
-                <Card.Title>
-                  <h5 className="card-title">Database Management</h5>
-                </Card.Title>
-                <div className="d-flex justify-content-center">
-                  <hr className="card-break" />
-                </div>
-                <Card.Text>
-                  <ul className="skills-list">
-                    <li className="skills-li">PostgreSQL</li>
-                    <li className="skills-li">MySQL</li>
-                    <li className="skills-li">Firestore</li>
-                  </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col className="d-flex justify-content-center">
-            <Card className="skill-card">
-              <Card.Body className="pt-2 px-2">
-                <Card.Title>
-                  <h5 className="card-title">Web Development</h5>
-                </Card.Title>
-                <div className="d-flex justify-content-center">
-                  <hr className="card-break" />
-                </div>
-                <Card.Text>
-                  <ul className="skills-list">
-                    <li className="skills-li">HTML/CSS/JavaScript</li>
-                    <li className="skills-li">ReactJS</li>
-                    <li className="skills-li">Firebase</li>
-                  </ul>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
+        <Row className="w-100">
+          <div className="skills-container">
+            <div className="skill-card">
+              <h5 className="card-title">Programming Languages</h5>
+              <hr className="card-break" />
+              <ul className="skills-list">
+                <li className="skills-li">C/C++</li>
+                <li className="skills-li">Java</li>
+                <li className="skills-li">Python</li>
+              </ul>
+            </div>
+            <div className="skill-card">
+              <h5 className="card-title">AI / Machine Learning</h5>
+              <hr className="card-break" />
+              <ul className="skills-list">
+                <li className="skills-li">PyTorch</li>
+                <li className="skills-li">TensorFlow</li>
+              </ul>
+            </div>
+            <div className="skill-card">
+              <h5 className="card-title">Hardware</h5>
+              <hr className="card-break" />
+              <ul className="skills-list">
+                <li className="skills-li">Verilog / HLS</li>
+                <li className="skills-li">Arduino</li>
+                <li className="skills-li">Raspberry Pi</li>
+                <li className="skills-li">ARM</li>
+              </ul>
+            </div>
+            <div className="skill-card">
+              <h5 className="card-title">Database</h5>
+              <hr className="card-break" />
+              <ul className="skills-list">
+                <li className="skills-li">PostgreSQL</li>
+                <li className="skills-li">MySQL</li>
+                <li className="skills-li">Firestore</li>
+              </ul>
+            </div>
+            <div className="skill-card">
+              <h5 className="card-title">Web Development</h5>
+              <hr className="card-break" />
+              <ul className="skills-list">
+                <li className="skills-li">HTML / CSS / JavaScript</li>
+                <li className="skills-li">ReactJS</li>
+                <li className="skills-li">Bootstrap</li>
+                <li className="skills-li">Firebase</li>
+              </ul>
+            </div>
+          </div>
         </Row>
       </Container>
     );
