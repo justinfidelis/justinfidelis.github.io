607ab114687d37a53efae6c040299dc61efc9875
Justin Wong <justinfidelis@gmail.com>
2023-07-15 12:57:20 +0800

    create variables for colors

--- portfolio/src/components/contact/EmailIcon.jsx
+++ portfolio/src/components/contact/EmailIcon.jsx
@@ -2,6 +2,9 @@ import React, {useState} from 'react';
 
 const EmailLogo = () => {
     const [isHover, setIsHover] = useState(false);
+    const strokecolor = "cornsilk";
+    const hoverColor = "#8AFFF1";
+
     return (
       <a
         className="contact-logo-container"
@@ -14,7 +17,7 @@ const EmailLogo = () => {
             <path
               className="contact-logo-path"
               d="M 7.6027863,17.344897 40.000003,44.529395 v 0 L 72.397221,17.344896"
-              stroke={isHover ? "#8AFFF1" : "#ffffff"}
+              stroke={isHover ? hoverColor : strokecolor}
               strokeWidth="2.2"
               strokeLinecap="butt"
               strokeLinejoin="miter"
@@ -25,7 +28,7 @@ const EmailLogo = () => {
             <path
               className="contact-logo-path"
               d="M 34.601977,40.000088 7.6030299,62.654898"
-              stroke={isHover ? "#8AFFF1" : "#ffffff"}
+              stroke={isHover ? hoverColor : strokecolor}
               strokeWidth="2.2"
               strokeLinecap="butt"
               strokeLinejoin="miter"
@@ -36,7 +39,7 @@ const EmailLogo = () => {
             <path
               className="contact-logo-path"
               d="M 45.398022,40.000088 72.39697,62.654898"
-              stroke={isHover ? "#8AFFF1" : "#ffffff"}
+              stroke={isHover ? hoverColor : strokecolor}
               strokeWidth="2.2"
               strokeLinecap="butt"
               strokeLinejoin="miter"
@@ -47,7 +50,7 @@ const EmailLogo = () => {
             <path
               className="contact-logo-path"
               d="m 11,16 h 58 c 2.77,0 5,2.23 5,5 v 38 c 0,2.77 -2.23,5 -5,5 H 11 C 8.23,64 6,61.77 6,59 V 21 c 0,-2.77 2.23,-5 5,-5 z"
-              stroke={isHover ? "#8AFFF1" : "#ffffff"}
+              stroke={isHover ? hoverColor : strokecolor}
               strokeWidth="2.2"
               strokeLinecap="round"
               strokeLinejoin="round"
